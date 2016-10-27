bfd57f7c73b6d0c27f6a39bad6fc54e44ab51e0f
MossFrog <kaanozcelik@sabanciuniv.edu>
2016-10-27 12:16:42 +0300

    Added extra edge and corner detection scripts created for the image processing and computer vision laboratories at Sabanci University

---
+++ Edge and Corner Detection/cornerDetection.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+#Shi-Tomasi Corner Detection Section
+img = cv2.imread('simple.jpg')
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+corners = cv2.goodFeaturesToTrack(gray,25,0.01,10)
+corners = np.int0(corners)
+
+for i in corners:
+    x,y = i.ravel()
+    cv2.circle(img,(x,y),3,255,-1)
+
+plt.imshow(img),plt.show()
+
+#Harris -Stephens Corner Detection Section
+filename = 'checker.png'
+img = cv2.imread(filename)
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+gray = np.float32(gray)
+dst = cv2.cornerHarris(gray,2,3,0.04)
+
+dst = cv2.dilate(dst,None)
+
+img[dst>0.01*dst.max()]=[0,0,255]
+
+cv2.imshow('dst',img)
+if cv2.waitKey(0) & 0xff == 27:
+    cv2.destroyAllWindows()
---
+++ Min and Max Filters/localMinimum.py
@@ -0,0 +1,65 @@
+from scipy import signal as sg
+from PIL import Image
+import numpy as np
+import struct
+
+#-- Method for extracting RGB from Hexadecimal input
+def hexToRGB(inStr):
+    return struct.unpack('BBB',inStr.decode('hex'))
+
+#-- Definitions from 'http://juanreyero.com/article/python/python-convolution.html'
+#-- Modified for grayscale conversion.
+def np_from_img(fname):
+    return np.asarray(Image.open(fname).convert('L'), dtype=np.float32)
+def np_rgba_from_img(fname):
+    return np.asarray(Image.open(fname).convert('RGBA'), dtype=np.float32)
+
+def save_as_img(ar, fname):
+    Image.fromarray(ar.round().astype(np.uint8)).save(fname)
+
+def norm(ar):
+    return 255.*np.absolute(ar)/np.max(ar)
+
+
+def minimum(ar, wSize):
+    min = 255
+    for i in range(0, ar.shape[0] - wSize):
+        for j in range(0, ar.shape[1] - wSize):
+            for k in range(0, wSize):
+                for l in range(0, wSize):
+                  if(ar[i + k][j + l] < min):
+                      min = ar[i + k][j + l]
+                ar[i+wSize/2][j+wSize/2] = min
+                min = 255
+                
+    return ar
+
+#-- Common size method, scales the matrices to equal proportions
+def common_size(a1, a2):
+    (r1, c1) = a1.shape
+    (r2, c2) = a2.shape
+    return (a1[r1-r2 if r1>r2 else 0:,
+               c1-c2 if c1>c2 else 0:],
+            a2[r2-r1 if r2>r1 else 0::,
+               c2-c1 if c2>c1 else 0:])
+
+#-- Request the file name from the user
+
+print("The image must be located within the directory of the Python Script.")
+fname = input('Enter the file name: ')
+wSize = input('Enter the window size: ')
+
+#-- Open and convert the image to a Numpy array
+sourceImage = np_from_img(fname)
+
+#-- Remove the image suffix from fname
+fname = fname[:fname.find(".")]
+
+#-- Uncomment to save the unedited Grayscale version
+#save_as_img(norm(sourceImage), fname + "-Grayscale.png")
+
+#-- Apply convolution and save
+modImage = minimum(sourceImage, wSize)
+save_as_img(sourceImage,
+            fname + "-Minimum.png")
+
